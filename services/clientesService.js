18a0ccc2346af6ef85a3b403f353b8652c8d0ea1
Marcos Ojeda Escalante <marcos.ojeda.escalante@gmail.com>
2023-06-30 21:39:09 -0400

    eliminar back implementado

--- services/clientesService.js
+++ services/clientesService.js
@@ -45,8 +45,26 @@ const actualizar = (cliente) => {
 }
 
 const eliminar = (id) => {
-
+    const arregloClientes = [];
+    let datos = fs.readFileSync(`./datos/customers.csv`, 'utf-8');
+    datos = datos.replace(/\r/g, '');
+    datos = datos.replace(/\n/g, ';');
+    datos = datos.split(';');
+    datos.forEach((element, indice) => {
+        if ((indice + 1) % 3 == 0) {
+            const cliente = new Cliente(datos[indice - 2], datos[indice - 1], datos[indice]);
+            arregloClientes.push(cliente);
+        }
+    });
+    const clientesFiltrados = arregloClientes.filter(elemento => elemento.clienteId != id);
+    let cadena = '';
+    clientesFiltrados.forEach((cliente) => {
+        cadena = `${cadena}${cliente.clienteId};${cliente.nombreDeCompania};${cliente.nombreDeContacto}\n`;
+    });
+    fs.writeFileSync('./datos/customers.csv', cadena);
 }
+
+eliminar('AAAAA');
 
 module.exports = {
     leerTodo,
